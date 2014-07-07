c32c04272ce6327f76f807815979176265ae93ca
xianritchie <christian.ritchie@pepsico.com>
2014-07-07 11:16:56 -0500

    Initial Commit

---
+++ CollectionViewModal/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CollectionViewModal
+//
+//  Created by Ritchie, Christian {BIS} on 7/7/14.
+//  Copyright (c) 2014 BIS Innovation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CollectionViewModal/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CollectionViewModal
+//
+//  Created by Ritchie, Christian {BIS} on 7/7/14.
+//  Copyright (c) 2014 BIS Innovation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
